80a08a46b9af35d0bd457ee29bf7ad718dab6574
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-04-09 12:42:51 -0500

    Inicializacion de proyecto

---
+++ src/main/java/com/lchumi/puertocom/PuertoComApplication.java
@@ -0,0 +1,13 @@
+package com.lchumi.puertocom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuertoComApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuertoComApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lchumi/puertocom/PuertoComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchumi.puertocom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuertoComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
